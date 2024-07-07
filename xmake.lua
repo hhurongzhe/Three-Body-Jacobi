b5c4d15fb68f50539709928ec947599ad51f1651
hurongzhe <90456443+hhurongzhe@users.noreply.github.com>
2024-07-07 22:20:31 +0800

    Delete xmake.lua

--- xmake.lua
+++
@@ -1,9 +0,0 @@
-add_rules("mode.release")
-add_requires("openmp")
-
-target("3b-jacobi")
-    set_kind("binary")
-    set_languages("c17", "c++17")
-    set_optimize("aggressive")
-    add_packages("openmp")
-    add_files("src/*.cpp")
